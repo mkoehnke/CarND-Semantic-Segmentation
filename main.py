a3268f0ce1b6384b72d56c1b92fb634268b46689
Mathias Köhnke <mathias.koehnke@daimler-x.com>
2017-12-27 11:31:21 +0100

    Reshape labels in the optimizer function

--- main.py
+++ main.py
@@ -94,8 +94,11 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
 
-    logits = tf.reshape(input, (-1, num_classes))
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
+
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+
     train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy_loss)
 
     return logits, train, cross_entropy_loss
